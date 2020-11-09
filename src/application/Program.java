23fbebbdc2826a94213399d6c9b97dea09aee3ec
fernandoFernandes101 <f_fernando@hotmail.com>
2020-11-09 15:26:45 -0300

    Acresci rotina de soma

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,10 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 7;
+		int z= x + y;
+		System.out.println(x + "+" + y + "=" + z);
 
 	}
 
